51dec87ad4de8ebf1b017210474a9ee09cd3bc81
qingyiyin <your_email@example.com>
2018-10-09 14:56:59 -0400

    Tested if hasThreeCardInSequence works properly

--- src/test/java/TwoOrMoreExchange.java
+++ src/test/java/TwoOrMoreExchange.java
@@ -20,4 +20,18 @@ public class TwoOrMoreExchange {
         List<Integer> index = testPlayer.getStrategy().isThreeOfSameSuit(testPlayer.getCards());
         this.isMatched = index.size() == 2;
     }
+
+    @Test
+    public void testThreeWithSameRank() {
+        Player testPlayer = new Player("S3 C3 H3 S7 S9");
+        List<Integer> index = testPlayer.getStrategy().isThreeOfSameRank(testPlayer.getCards());
+        this.isMatched = index.size() == 2;
+    }
+
+    @Test
+    public void testThreeInSquence() {
+        Player testPlayer = new Player("S3 C6 H8 S7 SJ");
+        List<Integer> index = testPlayer.getStrategy().hasThreeCardInSequence(testPlayer.getCards());
+        this.isMatched = index.size() == 2;
+    }
 }
