5df3c82122321c71ed98a0f9eec8b89c2f0859ad
qingyiyin <your_email@example.com>
2018-10-09 00:33:16 -0400

    Added DetectRank tests for testing the Card class

---
+++ src/test/java/DetectRank.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static junit.framework.TestCase.assertTrue;
+
+public class DetectRank {
+    @Test
+    public void testSuitsRankedCorrectly() {
+        Card card1 = new Card("H", 4);
+        Card card2 = new Card("H", 5);
+        Card card3 = new Card("S", 4);
+    }
+}
