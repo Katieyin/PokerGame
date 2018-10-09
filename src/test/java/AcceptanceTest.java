2dd051547400e3505ef59d206d3b5fe8c2c5cb8f
qingyiyin <your_email@example.com>
2018-10-09 15:39:10 -0400

    Added AcceptanceTest for basic combinations

--- src/test/java/AcceptanceTest.java
+++ src/test/java/AcceptanceTest.java
@@ -37,4 +37,34 @@ public class AcceptanceTest {
     public void testRoyalFlush() {
         assertEquals("Royal Flush", winners.get(0).getResult().getCombination());
     }
+
+    @Test
+//    For either player,  detect any straight flush and hold
+    public void testStraightFlush() {
+        assertEquals("Straight Flush", winners.get(1).getResult().getCombination());
+    }
+
+    @Test
+//    For either player,  detect any 4 of a kind and hold
+    public void testFourOfAKind() {
+        assertEquals("4 Of A Kind", winners.get(2).getResult().getCombination());
+    }
+
+    @Test
+//    For either player,  detect any full house and hold
+    public void testFullHouse() {
+        assertEquals("Full House", winners.get(3).getResult().getCombination());
+    }
+
+    @Test
+//    For either player,  detect any flush and hold
+    public void testFlush() {
+        assertEquals("Flush", winners.get(4).getResult().getCombination());
+    }
+
+    @Test
+//    For either player,  detect any straight  and hold
+    public void testStraight() {
+        assertEquals("Straight", winners.get(5).getResult().getCombination());
+    }
 }
