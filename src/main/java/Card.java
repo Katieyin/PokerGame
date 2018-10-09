eddf2d25544ac920605c358277ea21c99f0f3bd6
qingyiyin <your_email@example.com>
2018-10-09 00:37:06 -0400

    Added helper methods for printing cards

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -35,4 +35,32 @@ public class Card implements Comparable<Card> {
         else
             return this.getSuit().compareTo(card.getSuit());
     }
+
+
+    public boolean isGreaterThan(Card card) {
+        return this.compareTo(card) > 0;
+    }
+
+    public void printCard() {
+        switch (this.rank) {
+            case 1:
+            case 14:
+                System.out.print(this.suit + "A");
+                break;
+            case 11:
+                System.out.print(this.suit + "J");
+                break;
+            case 12:
+                System.out.print(this.suit + "Q");
+                break;
+            case 13:
+                System.out.print(this.suit + "K");
+                break;
+            default:
+                System.out.print(this.suit + this.rank);
+                break;
+
+        }
+
+    }
 }
