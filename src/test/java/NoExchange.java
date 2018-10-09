c7da3c518649eb1cff688c10209db2b72f12d2ab
qingyiyin <your_email@example.com>
2018-10-09 12:18:20 -0400

    Tested if isFourOfAKind works properly

--- src/test/java/NoExchange.java
+++ src/test/java/NoExchange.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
@@ -5,10 +6,20 @@ import static junit.framework.TestCase.assertTrue;
 public class NoExchange {
     private boolean isMatched;
 
+    @After
+    public void checkExchangeSize() {
+        assertTrue(this.isMatched);
+    }
     @Test
     public void testStraight() {
         Player testPlayer = new Player("S5 C4 H8 S7 D6");
         this.isMatched = testPlayer.getStrategy().isStraight(testPlayer.getCards()).isMatched();
-        assertTrue(this.isMatched);
+    }
+
+
+    @Test
+    public void testFourOfAKind() {
+        Player testPlayer = new Player("H4 H5 S4 C4 D4");
+        this.isMatched = testPlayer.getStrategy().isFourOfAKind(testPlayer.getCards()).isMatched();
     }
 }
