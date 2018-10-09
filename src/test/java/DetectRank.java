e54367466c068fe3408baa6d4235c9463a6cfcd7
qingyiyin <your_email@example.com>
2018-10-09 00:38:08 -0400

    Tested if the two card is correctly ranked

--- src/test/java/DetectRank.java
+++ src/test/java/DetectRank.java
@@ -14,4 +14,18 @@ public class DetectRank {
         assertTrue( greaterThan > 0);
         assertTrue( greaterThan2 > 0);
     }
+
+    @Test
+    public void testCardsRankedCorrectly() {
+        Card card1 = new Card("H", 4);
+        Card card2 = new Card("H", 5);
+        Card card3 = new Card("H", 3);
+        int lessThan = card1.compareTo(card2);
+        int greaterThan = card2.compareTo(card3);
+        int lessThan2 = card3.compareTo(card1);
+        assertTrue( lessThan < 0);
+        assertTrue( greaterThan > 0);
+        assertTrue( lessThan2 < 0);
+
+    }
 }
